ba4bcffdaaa387640801a843afb79d44883b1133
Phil Tsarik <phil.tsarik@gmail.com>
2014-07-30 15:30:05 +0300

    Remove redundant selectors

--- js/lightbox.js
+++ js/lightbox.js
@@ -278,8 +278,6 @@
 
       function postResize() {
         self.$lightbox.find('.lb-dataContainer').width(newWidth);
-        self.$lightbox.find('.lb-prevLink').height(newHeight);
-        self.$lightbox.find('.lb-nextLink').height(newHeight);
         self.showImage();
       }
 
